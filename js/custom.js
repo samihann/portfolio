965b5b725601500d1a8cbdc42cdee194ee059a07
Samihan <svn2998@gmail.com>
2022-01-04 19:09:54 -0600

    Updating index page

--- js/custom.js
+++ js/custom.js
@@ -37,33 +37,7 @@
 		if ($('.grid').length) {
 			new CBPGridGallery( document.getElementById( 'grid-gallery' ) );
 		}
-
-		/* ----------------------------------------------------------- */
-		/*  HIDE HEADER WHEN PORTFOLIO SLIDESHOW OPENED
-        /* ----------------------------------------------------------- */
-
-		$(".grid figure").on('click', function() {
-			$("#navbar-collapse-toggle").addClass('hide-header');
-		});
-
-		/* ----------------------------------------------------------- */
-		/*  SHOW HEADER WHEN PORTFOLIO SLIDESHOW CLOSED
-        /* ----------------------------------------------------------- */
-
-		$(".nav-close").on('click', function() {
-			$("#navbar-collapse-toggle").removeClass('hide-header');
-		});
-		$(".nav-prev").on('click', function() {
-			if ($('.slideshow ul li:first-child').hasClass('current')) {
-				$("#navbar-collapse-toggle").removeClass('hide-header');
-			}
-		});
-		$(".nav-next").on('click', function() {
-			if ($('.slideshow ul li:last-child').hasClass('current')) {
-				$("#navbar-collapse-toggle").removeClass('hide-header');
-			}
-		});
-
+		
 		/* ----------------------------------------------------------- */
 		/*  PORTFOLIO DIRECTION AWARE HOVER EFFECT
         /* ----------------------------------------------------------- */
